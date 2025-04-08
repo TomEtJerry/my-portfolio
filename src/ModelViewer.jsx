725be66ca8ced3ff10bed7c8ac735747d092bf49
TomEtJerry <tomsantoni1@gmail.com>
2025-04-08 16:15:50 +0200

    ok

--- src/ModelViewer.jsx
+++ src/ModelViewer.jsx
@@ -44,7 +44,7 @@ const ModelViewer = memo(({ modelPath }) => {
             {
                 root: null,
                 threshold: 0,
-                rootMargin: "300px", // ✅ Déclenche 300px avant d'entrer dans le viewport
+                rootMargin: "400px", // ✅ Déclenche 400px avant d'entrer dans le viewport
             }
         );
 
