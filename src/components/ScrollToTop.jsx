f5644157cbc0bd032968b2b78e5af539da8b31d5
TomEtJerry <tomsantoni1@gmail.com>
2025-05-13 14:49:58 +0200

    ok

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,13 +1,14 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 export default function ScrollToTop() {
-    const { pathname } = useLocation();
+    const { pathname } = useLocation()
+
     useEffect(() => {
         window.scrollTo(0, 0)
         ScrollTrigger.refresh()
-    }, [pathname]);
-    return null;
+    }, [pathname])
+    return null
 }
