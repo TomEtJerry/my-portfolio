809918840d52fe78a0385d03417297a5f518ca4b
TomEtJerry <tomsantoni1@gmail.com>
2025-05-13 16:15:31 +0200

    ok

--- src/components/ScrollTriggerRefresher.jsx
+++ src/components/ScrollTriggerRefresher.jsx
@@ -9,7 +9,7 @@ export default function ScrollTriggerRefresher() {
         // On laisse le temps à la nouvelle page de charger
         const timer = setTimeout(() => {
             ScrollTrigger.refresh();
-        }, 300); // 300ms suffisent pour recalculer les triggers après un changement de route
+        }, 1000); // 300ms suffisent pour recalculer les triggers après un changement de route
 
         return () => clearTimeout(timer);
     }, [pathname]);
